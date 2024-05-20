7b306a1c9b3dc683dc0f6d58413183c7336c2dd6
Carassius-2000 <skaratsev@gmail.com>
2024-05-20 22:34:27 +0300

    add self-hosting JS and CSS for docs

--- api.py
+++ api.py
@@ -9,6 +9,12 @@ from pydantic_models import (
     WorkerWithFeatures
 )
 from fastapi import FastAPI, HTTPException
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html
+)
+from fastapi.staticfiles import StaticFiles
 from databases import Database
 import uvicorn
 import joblib
@@ -51,7 +57,39 @@ async def lifespan(app: FastAPI) -> None:
     await database.disconnect()
 
 
-app = FastAPI(title="Stress Level Prediction", version="1.0.0", lifespan=lifespan)
+app = FastAPI(
+    title="Stress Level Prediction",
+    version="1.0.0",
+    lifespan=lifespan,
+    docs_url=None,
+    redoc_url=None
+)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css"
+    )
+
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="/static/redoc.standalone.js"
+    )
 
 
 def get_prediction(data) -> str:
